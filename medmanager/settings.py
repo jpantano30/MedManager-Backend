7d88be46b15934ba872fe745e31655777a1d85f4
Jena <jpantano30@gmail.com>
2024-07-08 11:58:01 -0400

    .

--- medmanager/settings.py
+++ medmanager/settings.py
@@ -71,7 +71,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     'https://medmanager.netlify.app',
